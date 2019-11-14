865f6f48b638513e236c47bc0f7ed9a411985aa1
Yi Zhao <yzhao@hubba.com>
2019-11-14 13:38:34 -0500

    async

--- deploy/index.js
+++ deploy/index.js
@@ -2,17 +2,19 @@ const core = require('@actions/core');
 const { exec } = require('@actions/exec');
 const github = require('@actions/github');
 
-try {
-  const serviceToDeploy = core.getInput('service');
-  
-  await exec('gcloud auth configure-docker');
-  await exec('cd ../');
-  await exec(
-    'git clone https://hubba-build:${{ secrets.GITHUB_PAT }}@github.com/hubba/infrastructure-2020.git'
-  );
-  await exec(`cd ${github.context.repo.name}`);
+(async () => {
+  try {
+    const serviceToDeploy = core.getInput('service');
 
-  await exec('cat ../hubba-infrastructure/scripts/deploy.sh');
-} catch (error) {
-  core.setFailed(error.message);
-}
+    await exec('gcloud auth configure-docker');
+    await exec('cd ../');
+    await exec(
+      'git clone https://hubba-build:${{ secrets.GITHUB_PAT }}@github.com/hubba/infrastructure-2020.git'
+    );
+    await exec(`cd ${github.context.repo.name}`);
+
+    await exec('cat ../hubba-infrastructure/scripts/deploy.sh');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
