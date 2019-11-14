198cfdbc5f411028ce29100888b4aef46ca57673
Yi Zhao <yzhao@hubba.com>
2019-11-14 13:35:51 -0500

    test git clone

--- deploy/index.js
+++ deploy/index.js
@@ -1,13 +1,17 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
+const { exec } = require('@actions/exec');
 const github = require('@actions/github');
 
 try {
   const serviceToDeploy = core.getInput('service');
-  console.log(`Hello ${serviceToDeploy}!`);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+
+  await exec('cd ../');
+  await exec(
+    'git clone https://hubba-build:${{ secrets.GITHUB_PAT }}@github.com/hubba/infrastructure-2020.git'
+  );
+  await exec(`cd ${github.context.repo.name}`);
+
+  await exec('cat ../hubba-infrastructure/scripts/deploy.sh');
 } catch (error) {
   core.setFailed(error.message);
 }
