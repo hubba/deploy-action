8db924f05e7e01c28b84eb4a274118c200b2baf7
Yi Zhao <yzhao@hubba.com>
2019-11-14 14:04:13 -0500

    pass cwd

--- deploy/index.js
+++ deploy/index.js
@@ -1,19 +1,18 @@
 const core = require('@actions/core');
 const { exec } = require('@actions/exec');
 const github = require('@actions/github');
+const path = require('path');
 
 (async () => {
   try {
     const serviceToDeploy = core.getInput('service');
-    const GITHUB_PAT = core.getInput('GITHUB_PAT')
+    const GITHUB_PAT = core.getInput('GITHUB_PAT');
 
     await exec('gcloud auth configure-docker --quiet');
-    process.chdir('../')
-    await exec(
-      `git clone https://hubba-build:${GITHUB_PAT}@github.com/hubba/infrastructure-2020.git`
-    );
-    process.chdir(`${github.context.repo.name}`);
-
+    await exec(`git clone https://hubba-build:${GITHUB_PAT}@github.com/hubba/infrastructure-2020.git`, [], {
+      cwd: path.resolve(process.cwd, '../')
+    });
+    console.log(process.cwd)
     await exec('cat ../hubba-infrastructure/scripts/deploy.sh');
   } catch (error) {
     core.setFailed(error.message);
