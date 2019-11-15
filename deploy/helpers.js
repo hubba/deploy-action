deae3ec2acd6855f87d74b94d4188d052a3785f2
Yi Zhao <yzhao@hubba.com>
2019-11-15 10:32:00 -0500

    add get service name

--- deploy/helpers.js
+++ deploy/helpers.js
@@ -1,4 +1,22 @@
-module.exports.getReviewAppUrl = (repo, branchName) => {
+function getServiceName(repo) {
+  const services = {
+    'prototype-2020': 'backend',
+    'prototype-2020-frontend': 'frontend',
+    frontend: 'brands',
+  };
+
+  if (repo in services) {
+    return services[repo];
+  }
+
+  return '';
+}
+
+function getReleaseName(serviceName, branchName) {
+  return `${serviceName}-${branchName.replace(/\//g, '-')}`.substring(0, 53);
+}
+
+function getReviewAppUrl(repo, branchName) {
   const serviceName = getServiceName(repo);
 
   const domain = serviceName === 'brands' ? 'brands-qa.hubba.com' : 'hubba.gold';
@@ -10,8 +28,10 @@ module.exports.getReviewAppUrl = (repo, branchName) => {
   return `https://${getReleaseName(serviceName, branchName)}.${
     serviceName !== 'brands' ? 'qa.' : ''
   }${domain}`;
-};
+}
 
-module.exports.getReleaseName = (serviceName, branchName) => {
-  return `${serviceName}-${branchName.replace(/\//g, '-')}`.substring(0, 53);
+module.exports = {
+  getServiceName,
+  getReviewAppUrl,
+  getReleaseName,
 };
