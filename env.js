ea1784b11efa6ae6ac605dff876ef30f0567dcdc
Yi Zhao <yzhao@hubba.com>
2019-11-16 20:56:12 -0500

    fix branch name

--- env.js
+++ env.js
@@ -1,11 +1,11 @@
 const github = require('@actions/github');
 
 function getEnv() {
-  // branch name is GITHUB_HEAD_REF for pull request
-  const [owner, repo] = process.env.GITHUB_HEAD_REF || process.env.GITHUB_REPOSITORY.split('/');
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
 
+  // branch name is GITHUB_HEAD_REF for pull request
   return {
-    branch: process.env.GITHUB_REF.replace('refs/heads/', ''),
+    branch: process.env.GITHUB_HEAD_REF || process.env.GITHUB_REF.replace('refs/heads/', ''),
     sha: github.context.payload.after,
     owner,
     repo,
