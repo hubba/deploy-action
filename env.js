097b490927c8b28268e1bf150341e7d96ce1ed3f
Yi Zhao <yzhao@hubba.com>
2019-11-16 20:52:25 -0500

    comment

--- env.js
+++ env.js
@@ -1,6 +1,7 @@
 const github = require('@actions/github');
 
 function getEnv() {
+  // branch name is GITHUB_HEAD_REF for pull request
   const [owner, repo] = process.env.GITHUB_HEAD_REF || process.env.GITHUB_REPOSITORY.split('/');
 
   return {
